6031c3564e2b951569aa98263e26612e9e60507a
Karl Merkli <info@karl-merkli.ch>
2017-08-13 15:09:51 +0200

    Started with detailed states

--- src/app/player/player-hero-detail/player-hero-detail.component.ts
+++ src/app/player/player-hero-detail/player-hero-detail.component.ts
@@ -1,4 +1,12 @@
 import {Component, OnInit, ChangeDetectionStrategy} from "@angular/core";
+import {State} from "../../reducers";
+import {Store} from "@ngrx/store";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {HeroStatFactory} from "../../hero/hero-stat.factory";
+import {TagStatFactory} from "../../hero/tag-stat.factory";
+import {PlayerStats} from "../state/player.resource";
+import {Observable} from "rxjs/Observable";
+import {getStats} from "../state/player.reducer";
 
 @Component({
   selector: "app-player-hero-detail",
@@ -8,9 +16,32 @@ import {Component, OnInit, ChangeDetectionStrategy} from "@angular/core";
 })
 export class PlayerHeroDetailComponent implements OnInit {
 
-  constructor() {}
+  public stats$: Observable<PlayerStats>;
+  public hero$: Observable<string>;
+  public playerId$: Observable<string>;
 
-  ngOnInit() {
+  constructor(
+    private store: Store<State>,
+    private route: ActivatedRoute,
+    private heroStat: HeroStatFactory,
+    private tagStat: TagStatFactory
+  ) {}
+
+  public ngOnInit() {
+    this.playerId$ = this.route.parent.paramMap
+      .map((params: ParamMap) => {
+        return params.get("playerId");
+      });
+
+    this.hero$ = this.route.paramMap
+      .map((params: ParamMap) => {
+        return params.get("hero");
+      });
+
+    this.stats$ = this.playerId$
+      .switchMap(playerId => {
+        return this.store.select(getStats(playerId))
+          .filter(data => Boolean(data));
+      });
   }
-
 }
